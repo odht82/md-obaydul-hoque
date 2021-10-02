d7acd691d1579419d76d6db84d2844d1b6598494
Obaydul Hoque <tusherh82@gmail.com>
2021-10-03 03:50:39 +0600

    construction page built

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          <span>Under construction</span><br /> Amaging website Waiting for release
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
